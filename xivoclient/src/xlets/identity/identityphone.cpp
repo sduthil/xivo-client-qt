70cbfd54f71da8109cbb7737d1f3c2072c4e5dd2
Jean-Yves LEBLEU <jylebleu@avencall.com>
2013-01-25 15:01:23 +0100

    merge

--- xivoclient/src/xlets/identity/identityphone.cpp
+++ xivoclient/src/xlets/identity/identityphone.cpp
@@ -38,14 +38,6 @@
 #include "phoneinfo.h"
 #include "channelinfo.h"
 
-
-<<<<<<< HEAD
-=======
-
-
-/*! \brief Constructor
- */
->>>>>>> 6f100b9d30e437a8c30a4e07b235d71af09be508
 IdentityPhone::IdentityPhone(QWidget * parent)
     : QWidget(parent)
 {
